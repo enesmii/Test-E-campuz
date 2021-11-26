ec4ae4e10cd260688054c9b855c5dac41781e99b
Enesmi Clarita <83512689+enesmii@users.noreply.github.com>
2021-11-26 16:41:54 +0700

    Create Login.php

---
+++ Login.php
@@ -0,0 +1,30 @@
+<html>
+
+<head>
+    <title>Login Simple Modular E-campuz</title>
+</head>
+<style>
+    .wrapper {
+        width: 500px;
+        margin: 20% auto;
+        padding: 30px;
+    }
+    form input {
+        width: 100%;
+        height: 40px;
+        padding: 5px;
+      border:1px;
+    }
+</style>
+
+          <body>
+              <div class="wrapper">
+                 <h2>Login</h2>
+                     <form action="proses_login.php" method="POST">
+                          <h5> input type="User name " placeholder="username" name="username" </h5>
+                          <h5> <input type="password" placeholder="password" name="password"> </h5>
+                          <h5> <input type="submit" value="Login"> </h4>
+                    </form>
+              </div>
+        </body>
+</html>
